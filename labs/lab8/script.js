3df6a8b6f80e889cf4c78eaa39a23a405dc58a32
Nikita Kumar <nikitak@berkeley.edu>
2024-11-22 11:49:58 -0600

    reorganizing files and making personal website

--- labs/lab8/script.js
+++
@@ -1,7 +0,0 @@
-function toggleTheme() {
-    let body = document.querySelector("body");
-    body.classList.toggle("dark-mode")
-}
-
-let buttonVariable = document.querySelector("#toggleButton")
-buttonVariable.onclick = toggleTheme;
